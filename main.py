06f6a2c6d7413943ad69ae4374fab52d390b8fe2
Данила Иванов <daniliv01@mail.ru>
2023-09-02 17:33:22 +0300

    Test get html

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import os
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+URL = "https://yandex.ru/"
+html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(html_page.text)
