fa1efc9d585444073cf3b0b6c0000248ecd11213
Данила Иванов <daniliv01@mail.ru>
2023-10-03 11:46:11 +0300

    add third script + fix

--- copy_dataset_random.py
+++ copy_dataset_random.py
@@ -1,23 +1,24 @@
-import csv
 import os
+import random
 import shutil
+import csv
 
-OUTPUT_FOLDER = "dataset_copy"
-CSV_FILE_NAME = "dataset_copy_annotations.csv"
+OUTPUT_FOLDER = "dataset_copy_random"
+CSV_FILE_NAME = "dataset_copy_random_annotations.csv"
 DATASET_FOLDER = "dataset"
 
 
-def copy_and_rename_dataset(dataset_folder, output_folder):
+def copy_and_rename_dataset_with_annotation(dataset_folder, output_folder):
     """
-    Копирует файлы из датасета в другую директорию, переименовывая их и создает файл аннотацию.
+    Копирует файлы из датасета в другую директорию, переименовывая их случайным номером и создает файл аннотацию.
 
     :param dataset_folder: Путь к папке с датасетом
     :param output_folder: Путь куда будет скопирован датасет
     :return:
     """
 
-    if not os.path.exists(OUTPUT_FOLDER):
-        os.makedirs(OUTPUT_FOLDER)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     # Создаем CSV-файл для аннотаций
     annotation_file = os.path.join(output_folder, CSV_FILE_NAME)
@@ -25,17 +26,18 @@ def copy_and_rename_dataset(dataset_folder, output_folder):
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(['absolute_path', 'relative_path', 'class_label'])
 
-        # Проходим по всем файлам в датасете
         for root, _, files in os.walk(dataset_folder):
-            for file_index, file in enumerate(files):
+            for file in files:
                 # Получаем путь к файлу
                 path = os.path.join(root, file)
 
                 # Получаем метку класса (имя папки)
                 class_label = os.path.basename(root)
 
-                # Создаем новое имя файла с меткой класса и порядковым номером
-                new_filename = f"{class_label}_{str(file_index).zfill(4)}{os.path.splitext(file)[-1]}"
+                random_number = random.randint(0, 10000)
+
+                # Создаем новое имя файла с номером и расширением .jpg
+                new_filename = f"{random_number}.jpg"
 
                 # Полный путь к новому файлу в выходной директории
                 new_file_path = os.path.join(output_folder, new_filename)
@@ -51,8 +53,7 @@ def copy_and_rename_dataset(dataset_folder, output_folder):
 
 
 if __name__ == "__main__":
-    # Вызываем функцию для копирования и переименования датасета
-    copy_and_rename_dataset(DATASET_FOLDER, OUTPUT_FOLDER)
+    copy_and_rename_dataset_with_annotation(DATASET_FOLDER, OUTPUT_FOLDER)
 
     print(f"Датасет успешно скопирован и переименован в {OUTPUT_FOLDER}")
 
